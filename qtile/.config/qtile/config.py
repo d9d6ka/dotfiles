6db22bde661dd66cff71c4dc23e0a9870157dcc1
Vadim Zyamalov <zyamalov.ve@gmail.com>
2020-12-11 21:07:48 +0300

    U: qtile config

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -44,139 +44,225 @@ def xresget(xres, key, fallback):
         return fallback
 
 
-def window_to_prev_group(qtile):
-    if qtile.currentWindow is not None:
-        i = qtile.groups.index(qtile.currentGroup)
-        qtile.currentWindow.togroup(qtile.groups[i - 1].name)
-
-
-def window_to_next_group(qtile):
-    if qtile.currentWindow is not None:
-        i = qtile.groups.index(qtile.currentGroup)
-        qtile.currentWindow.togroup(qtile.groups[i + 1].name)
-
-
+@lazy.function
 def window_to_previous_screen(qtile):
     i = qtile.screens.index(qtile.current_screen)
     if i != 0:
-        group = qtile.screens[i - 1].group.name
+        group = qtile.screens[i-1].group.name
         qtile.current_window.togroup(group)
 
 
+@lazy.function
 def window_to_next_screen(qtile):
     i = qtile.screens.index(qtile.current_screen)
     if i + 1 != len(qtile.screens):
-        group = qtile.screens[i + 1].group.name
+        group = qtile.screens[i+1].group.name
         qtile.current_window.togroup(group)
 
 
+@lazy.function
 def switch_screens(qtile):
     i = qtile.screens.index(qtile.current_screen)
-    group = qtile.screens[i - 1].group
+    group = qtile.screens[i-1].group
     qtile.current_screen.set_group(group)
 
 
 # --- Colors ---
 xresources = xrp.parse_file(
-        '{}/.Xresources'.format(os.path.expanduser('~')),
-        encoding='utf8'
-        )
+    '{}/.Xresources'.format(os.path.expanduser('~')),
+    encoding='utf8'
+)
 colors = {
-        'fg': xresget(xresources, '*.foreground', '#FFFFFF'),
-        'selfg': xresget(xresources, '*.foreground', '#FFFFFF'),
-        'bg': xresget(xresources, '*.background', '#000000'),
-        'selbg': xresget(xresources, '*.color8', '#282828'),
-        }
+    'fg':    xresget(xresources, '*.foreground', '#FFFFFF'),
+    'selfg': xresget(xresources, '*.foreground', '#FFFFFF'),
+    'bg':    xresget(xresources, '*.background', '#000000'),
+    'selbg': xresget(xresources, '*.color8',     '#1050A0'),
+}
 
 # --- Defaults ---
 mod = "mod4"
 terminal = guess_terminal()
+home = os.path.expanduser('~')
 
 # --- Keyboard and Mouse shortcuts ---
 keys = [
     # Switch between windows in current stack pane
-    Key([mod], "k", lazy.layout.down(),
-        desc="Move focus down in stack pane"),
-    Key([mod], "j", lazy.layout.up(),
-        desc="Move focus up in stack pane"),
+    Key(
+        [mod], "k",
+        lazy.layout.down(),
+        desc="Move focus down in stack pane"
+    ),
+    Key(
+        [mod], "j",
+        lazy.layout.up(),
+        desc="Move focus up in stack pane"
+    ),
 
     # Move windows up or down in current stack
-    Key([mod, "shift"], "k", lazy.layout.shuffle_down(),
-        desc="Move window down in current stack "),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_up(),
-        desc="Move window up in current stack "),
+    Key(
+        [mod, "shift"], "k",
+        lazy.layout.shuffle_down(),
+        desc="Move window down in current stack"
+    ),
+    Key(
+        [mod, "shift"], "j",
+        lazy.layout.shuffle_up(),
+        desc="Move window up in current stack"
+    ),
+
+    # Move windows between screens
+    Key(
+        [mod, "shift", "control"], "j",
+        window_to_previous_screen,
+        desc="Move window to the previous screen"
+    ),
+    Key(
+        [mod, "shift", "control"], "k",
+        window_to_next_screen,
+        desc="Move window to th next screen"
+    ),
+
+    # Rotate screens
+    Key(
+        [mod, "shift", "control"], "space",
+        switch_screens,
+        desc="Rotate screens"
+    ),
 
     # Toggle between different layouts as defined below
-    Key([mod, "control"], "comma", lazy.prev_layout(), desc="Toggle between layouts"),
-    Key([mod, "control"], "period", lazy.next_layout(), desc="Toggle between layouts"),
+    Key(
+        [mod, "control"], "comma",
+        lazy.prev_layout(),
+        desc="Toggle between layouts"
+    ),
+    Key(
+        [mod, "control"], "period",
+        lazy.next_layout(),
+        desc="Toggle between layouts"
+    ),
 
     # Kill window
-    Key([mod, "shift"], "c", lazy.window.kill(), desc="Kill focused window"),
+    Key(
+        [mod, "shift"], "c",
+        lazy.window.kill(),
+        desc="Kill focused window"
+    ),
 
     # Sound and Power
-    Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%"), desc="Lower volume"),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%"), desc="Raise volume"),
-    Key([], "XF86AudioMute", lazy.spawn("sh -c 'pactl set-sink-mute @DEFAULT_SINK@ toggle'"), desc="Mute"),
-    Key([], "XF86PowerOff", lazy.spawn("powermenu"), desc="Powermenu"),
+    Key(
+        [], "XF86AudioLowerVolume",
+        lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%"),
+        desc="Lower volume"
+    ),
+    Key(
+        [], "XF86AudioRaiseVolume",
+        lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%"),
+        desc="Raise volume"
+    ),
+    Key(
+        [], "XF86AudioMute",
+        lazy.spawn("sh -c 'pactl set-sink-mute @DEFAULT_SINK@ toggle'"),
+        desc="Mute"
+    ),
+    Key(
+        [], "XF86PowerOff",
+        lazy.spawn("powermenu"),
+        desc="Powermenu"
+    ),
 
-    # Launch terminal and rofi
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    Key([mod, "shift"], "Return", lazy.spawn("rofi -show drun -show-icons"),
-        desc="Launch rofi -show drun"),
-    Key([mod, "shift", "control"], "Return", lazy.spawn("rofi -show run"),
-        desc="Launch rofi -show run"),
+    # Launch terminal and other programs
+    Key(
+        [mod], "Return",
+        lazy.spawn(terminal),
+        desc="Launch terminal"
+    ),
+    Key(
+        [mod, "shift"], "Return",
+        lazy.spawn("rofi -show drun -show-icons"),
+        desc="Launch rofi -show drun"
+    ),
+    Key(
+        [mod, "shift", "control"], "Return",
+        lazy.spawn("rofi -show run"),
+        desc="Launch rofi -show run"
+    ),
+    Key(
+        [mod], "c",
+        lazy.spawn("clipmenu -p 'Clip:'"),
+        desc="Launch clipmenu"
+    ),
 
-    Key([mod, "shift", "control"], "q", lazy.restart(), desc="Restart qtile"),
-    Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown qtile"),
-    Key([mod], "r", lazy.spawncmd(),
-        desc="Spawn a command using a prompt widget"),
+    # Restart/Close qtile
+    Key(
+        [mod, "shift", "control"], "q",
+        lazy.restart(),
+        desc="Restart qtile"
+    ),
+    Key(
+        [mod, "shift"], "q",
+        lazy.shutdown(),
+        desc="Shutdown qtile"
+    ),
+    Key(
+        [mod], "r",
+        lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"
+    ),
 ]
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([mod], "Button2", lazy.window.bring_to_front())
+    Drag(
+        [mod], "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position()
+    ),
+    Drag(
+        [mod], "Button3",
+        lazy.window.set_size_floating(),
+        start=lazy.window.get_size()
+    ),
+    Click(
+        [mod], "Button2",
+        lazy.window.bring_to_front()
+    )
 ]
 
 # --- Groups ---
 group_names = [
-        ('1', {'layout': 'monadtall'}),
-        ('2', {'layout': 'monadtall'}),
-        ('3', {'layout': 'monadtall'}),
-        ('4', {'layout': 'monadtall'}),
-        ('5', {'layout': 'monadtall'}),
-        ('6', {'layout': 'monadtall'}),
-        ('7', {'layout': 'monadtall'}),
-        ('8', {'layout': 'monadtall'})
-        ]
+    ('1', {'layout': 'monadtall'}),
+    ('2', {'layout': 'monadtall'}),
+    ('3', {'layout': 'monadtall'}),
+    ('4', {'layout': 'monadtall'}),
+    ('5', {'layout': 'monadtall'}),
+    ('6', {'layout': 'monadtall'}),
+    ('7', {'layout': 'monadtall'}),
+    ('8', {'layout': 'monadtall'})
+]
 
 groups = [Group(i, **kwargs) for i, kwargs in group_names]
 
 for i in groups:
     keys.extend([
-        # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen(),
-            desc="Switch to group {}".format(i.name)),
-
-        # mod1 + shift + letter of group = switch to & move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
-            desc="Switch to & move focused window to group {}".format(i.name)),
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-        #     desc="move focused window to group {}".format(i.name)),
+        Key(
+            [mod], i.name,
+            lazy.group[i.name].toscreen(),
+            desc="Switch to group {}".format(i.name)
+        ),
+        Key(
+            [mod, "shift"], i.name,
+            lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name)
+        ),
     ])
 
 # --- Layouts ---
 layout_theme = {
-        "border_width": 1,
-        "margin": 2,
-        "border_focus": colors['fg'],
-        "border_normal": colors['bg']
-        }
+    "border_width": 1,
+    "margin": 2,
+    "border_focus": colors['fg'],
+    "border_normal": colors['bg']
+}
 
 layouts = [
     layout.MonadTall(**layout_theme),
@@ -202,26 +288,24 @@ widget_defaults = dict(
 extension_defaults = widget_defaults.copy()
 
 widget_list = [
-        widget.CurrentLayoutIcon(),
-        widget.GroupBox(
-            borderwidth=1,
-            rounded=False,
-            hide_unused=True,
-            foreground=colors['fg'],
-            highlight_method='line',
-            highlight_color=[colors['selbg'], colors['selbg']],
-            urgent_alert_method='line',
-            urgent_text=colors['bg'],
-            urgent_border=colors['fg']
-            ),
-        widget.Prompt(),
-        widget.WindowName(),
-        widget.Systray(),
-        widget.Clock(
-            format='%H:%M',
-            mouse_callbacks={'Button1': lambda qtile: qtile.cmd_spawn('gsimplecal')}
-            )
-        ]
+    widget.CurrentLayoutIcon(),
+    widget.GroupBox(
+        borderwidth=1,
+        rounded=False,
+        hide_unused=True,
+        foreground=colors['fg'],
+        highlight_method='line',
+        highlight_color=[colors['selbg'], colors['selbg']],
+        urgent_alert_method='line',
+        urgent_text=colors['bg'],
+        urgent_border=colors['fg']),
+    widget.Prompt(),
+    widget.WindowName(),
+    widget.Systray(),
+    widget.Clock(
+        format='%H:%M',
+        mouse_callbacks={'Button1': lambda qtile: qtile.cmd_spawn('gsimplecal')})
+]
 
 # --- Screens ---
 screens = [
@@ -229,9 +313,7 @@ screens = [
         bottom=bar.Bar(
             widget_list,
             20,
-            background=colors['bg']
-        ),
-    ),
+            background=colors['bg'])),
 ]
 
 # --- Rules and Settings ---
@@ -262,11 +344,26 @@ floating_layout = layout.Floating(float_rules=[
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 
+class_group = {
+    'Firefox': '8',
+    'qutebrowser': '8'
+}
 
-# --- Autostart ---
+
+# --- Hooks ---
+@hook.subscribe.client_new
+def to_group(window):
+    wm_class = window.cmd_inspect()['wm_class']
+    for wmc in wm_class:
+        if wmc in class_group.keys():
+            window.cmd_togroup(class_group[wmc])
+            break
+
+
 @hook.subscribe.startup
 def start():
-    subprocess.call(['{}/.config/qtile/autostart.sh'.format(os.path.expanduser('~'))])
+    subprocess.call(['{}/.config/qtile/autostart.sh'.format(home)])
+
 
 # XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
 # string besides java UI toolkits; you can see several discussions on the
