73827e7a5c11e8408d03298ad439b3279964aec1
Vadim Zyamalov <zyamalov.ve@gmail.com>
2020-12-17 11:50:29 +0300

    U: fish prompt fixes

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -4,39 +4,52 @@ function __git_status
     if test $status -ne 0
         return 1
     end
-    
+
+    # Set prefix for git part
+    set -l git_prefix ' on '
+
+    set -l git_correct
     set -l git_remote (git remote -v | grep '(push)' | head -n1 | awk '{print $1}')
-    set -l git_branch
+    set -l git_branch (git branch --show-current)
     set -l git_remote_branch
     set -l git_dirty
     set -l git_color
-    set -l git_prefix
-    
-    # Set prefix for git part
-    set git_prefix ' on '
-    
-    # Verify remote branch
-    set git_branch (git branch --show-current)
-    if test (git branch --all | grep -c "remotes/$git_remote/$git_branch\$") -gt 0
-        set git_remote_branch $git_branch
-    else if test (git branch --all | grep -c "remotes/$git_remote/main\$") -gt 0
-        set git_remote_branch main
+
+    if set -q git_remote
+        set git_correct 0
     else
-        set git_remote_branch master
+        set git_correct 1
+        set git_dirty "?"
     end
-        
-    # Check dirty/unpushed
-    if test (git log $git_branch..$git_remote/$git_remote_branch | grep -c "commit") -gt 0
-        set git_color yellow
-        set git_dirty '<'
-    else if test (git status -unormal --porcelain | wc -l) -gt 0
-        set git_color $fish_color_status
-        set git_dirty '*'
-    else if test (git log $git_remote/$git_remote_branch..$git_branch | grep -c "commit") -gt 0
-        set git_color $fish_color_user
-        set git_dirty '>'
-    else
-        set git_color $fish_color_user
+
+    if test (git branch --all | grep -c "remotes") -eq 0
+        set git_correct 1
+        set git_dirty "?"
+    end
+
+    if test $git_correct -eq 0
+        # Verify remote branch
+        if test $git_correct -eq 0 -a (git branch --all | grep -c "remotes/$git_remote/$git_branch\$") -gt 0
+            set git_remote_branch $git_branch
+        else if test $git_correct -eq 0 -a (git branch --all | grep -c "remotes/$git_remote/main\$") -gt 0
+            set git_remote_branch main
+        else
+            set git_remote_branch master
+        end
+
+        # Check dirty/unpushed
+        if test (git log $git_branch..$git_remote/$git_remote_branch | grep -c "commit") -gt 0
+            set git_color yellow
+            set git_dirty '<'
+        else if test (git status -unormal --porcelain | wc -l) -gt 0
+            set git_color $fish_color_status
+            set git_dirty '*'
+        else if test (git log $git_remote/$git_remote_branch..$git_branch | grep -c "commit") -gt 0
+            set git_color $fish_color_user
+            set git_dirty '>'
+        else
+            set git_color $fish_color_user
+        end
     end
     
     # Print git branch info
