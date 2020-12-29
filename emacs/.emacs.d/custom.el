d804b38a2a2000677104d4257ebb71265c79b152
Vadim Zyamalov <zyamalov.ve@gmail.com>
2020-12-29 11:49:23 +0300

    U: emacs config... again

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-jedi zenburn-theme which-key use-package rainbow-delimiters python-mode nord-theme lsp-pyright evil-collection doom-modeline company-lsp company-box auctex)))
+   '(lsp-pyright company-box company lsp-mode auctex zenburn-theme nord-theme which-key rainbow-delimiters evil-collection evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
