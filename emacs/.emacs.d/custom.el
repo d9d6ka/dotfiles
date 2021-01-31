1fa83e655e971275c73f23e1aedf56fbd0367d69
Vadim Zyamalov <zyamalov.ve@gmail.com>
2021-01-31 22:05:42 +0300

    U: emacs custom.el

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(auctex lsp-pyright company-box company lsp-mode poly-org polymode counsel swiper ivy-posframe ivy which-key undo-tree evil-collection evil zenburn-theme nord-theme rainbow-delimiters no-littering use-package))
  '(safe-local-variable-values
    '((eval add-hook 'after-save-hook
            (lambda nil
